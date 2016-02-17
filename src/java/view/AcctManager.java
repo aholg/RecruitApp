e784d8469a52e24b16579c504bc41aa349ac340e
aholg <holgersson31@hotmail.com>
2016-02-17 13:54:16 +0100

    Test commit

--- src/java/view/AcctManager.java
+++ src/java/view/AcctManager.java
@@ -5,7 +5,6 @@
  */
 package view;
 
-
 /**
  *
  * @author angie
@@ -28,8 +27,8 @@ import model.AccException;
 
 @Named("AcctManager")
 @ConversationScoped
-public class AcctManager implements Serializable{
-    
+public class AcctManager implements Serializable {
+
     @EJB
     private AccHandler accHandler;
     private Exception accountException;
@@ -40,13 +39,13 @@ public class AcctManager implements Serializable{
     private String loginPassword;
     private Exception transactionFailure;
     private boolean loggedIn = false;
-     @Inject
+    @Inject
     private Conversation conversation;
     private Exception loginFailure;
     private String loginResult;
-    
+
     public boolean isUserIsLoggedIn() {
-        if(SessionData.getSession() == null){ //Check first if there even exists a session
+        if (SessionData.getSession() == null) { //Check first if there even exists a session
             return false;
         }
         Object session = SessionData.getSession().getAttribute("username"); // Check if there exists a username
@@ -54,9 +53,6 @@ public class AcctManager implements Serializable{
         return name != null;
     }
 
-   
-    
-    
     private void startConversation() {
         if (conversation.isTransient()) {
             conversation.begin();
@@ -74,48 +70,47 @@ public class AcctManager implements Serializable{
         e.printStackTrace(System.err);
         transactionFailure = e;
     }
-        public boolean getSuccess() {
+
+    public boolean getSuccess() {
         return accountException == null;
     }
-    
-    
-    public void setRegisterUsername(String registerUsername){
+
+    public void setRegisterUsername(String registerUsername) {
         this.registerUsername = registerUsername;
     }
-    
-    public String getRegisterUsername(){
+
+    public String getRegisterUsername() {
         return registerUsername;
     }
-    
-     public void setLoginUsername(String loginUsername){
+
+    public void setLoginUsername(String loginUsername) {
         this.loginUsername = loginUsername;
     }
-    
-    public String getLoginUsername(){
+
+    public String getLoginUsername() {
         return loginUsername;
     }
-    
-    public void setRegisterPassword(String registerPassword){
+
+    public void setRegisterPassword(String registerPassword) {
         this.registerPassword = registerPassword;
     }
-    
-    public String getRegisterPassword(){
+
+    public String getRegisterPassword() {
         return registerPassword;
     }
-    
-    public void setLoginPassword(String loginPassword){
+
+    public void setLoginPassword(String loginPassword) {
         this.loginPassword = loginPassword;
     }
-    
-    public String getLoginPassword(){
+
+    public String getLoginPassword() {
         return loginPassword;
     }
-    
- 
 
     private String jsf22Bugfix() {
         return "";
     }
+
     public String login() {
         startConversation();
         FacesContext context = FacesContext.getCurrentInstance();
@@ -123,127 +118,90 @@ public class AcctManager implements Serializable{
         try {
             request.login(this.loginUsername, this.loginPassword);
         } catch (ServletException e) {
-             loginResult="Fail";
-            context.addMessage(null, new FacesMessage("Login failed." +e.getMessage()+ loginUsername + loginPassword));
+            loginResult = "Fail";
+            context.addMessage(null, new FacesMessage("Login failed." + e.getMessage() + loginUsername + loginPassword));
             return "fail";
         }
-        if(accHandler.checkRole(loginUsername)==true){
-           return "successRecruiter";
-         
-        }else return "successApplicant";
-        
-       //if(accHandler.checkRole(loginUsername)==true){
-         //  return "Recruiter.xhtml?faces-redirect=true";
-           
-       //}else return "Applicant.xhtml?faces-redirect=true";
-       
+        if (accHandler.checkRole(loginUsername) == true) {
+            return "successRecruiter";
 
-        
-    }
-    public String Validity() {
-    return loginResult;
-}
-     /**
-    * The method handles the login logic.
-    * the method calls and passes entered username and password to
-    * loginAccount(username,password) method in the controller.
-    * 
-    * if the loginAccount method returns false then this method throws an exception,
-    * otherwise the method sets up a http session. The session variable contains
-    * user information such as user's "username"
-    * 
-    * @return empty string, to handle jsf bug 
-    */
-    public String loginUser(){
-       startConversation();
-        
-       if(accHandler.loginAccount(this.getLoginUsername(), this.getLoginPassword()) == null){
-           handleException(new Exception("Please make sure Username and Password are correct"));
-           return jsf22Bugfix();
-       }
-        
-        // set up a http session and set username as an attribute
-       // HttpSession session = SessionData.getSession();
-      //  session.setAttribute("username", getLoginUsername());
+        } else {
+            return "successApplicant";
+        }
 
-        //reset the jsf fields 
-        loginUsername="";
-        loginPassword="";
-       return jsf22Bugfix();
     }
-    
-    
+
+
+
     /**
-    * The registerUser() handles the logic for creating a new account. 
-    * The method takes the entered user information and calls the createAccount method
- in the controller(AccHandler.java) which creates an account for the user by saving
- the information in the database.
- 
- After registering process is done successfully the user gets logged in automatically.
-    * 
-    * @return empty string, to handle jsf bug 
-    */
-    public String registerUser(){
+     * The registerUser() handles the logic for creating a new account. The
+     * method takes the entered user information and calls the createAccount
+     * method in the controller(AccHandler.java) which creates an account for
+     * the user by saving the information in the database.
+     *
+     * After registering process is done successfully the user gets logged in
+     * automatically.
+     *
+     * @return empty string, to handle jsf bug
+     */
+    public String registerUser() {
         startConversation();
         // check if the entered username and password is less than 1 character then throw an exception 
-        if(!(this.getRegisterUsername().length() > 0 && this.getRegisterPassword().length() > 0)){   //Check if the name and the password has a size bigger then 0
+        if (!(this.getRegisterUsername().length() > 0 && this.getRegisterPassword().length() > 0)) {   //Check if the name and the password has a size bigger then 0
             handleException(new Exception("The usernamne or the password needs to be atleast one character long"));
             return jsf22Bugfix();
         }
-        try{
+        try {
             // call createAccount in the accountHandler to create an account.
-            accHandler.createAccount(this.getRegisterUsername(),sha256(this.getRegisterPassword()));
-        }
-        catch(AccException ex){
+            accHandler.createAccount(this.getRegisterUsername(), sha256(this.getRegisterPassword()));
+        } catch (AccException ex) {
             handleException(ex);
             return jsf22Bugfix();
         }
-        
-        // set up an session for the new user
-       // HttpSession session = SessionData.getSession();
-       // session.setAttribute("username", getRegisterUsername()); //Set session attribute
 
+        // set up an session for the new user
+        // HttpSession session = SessionData.getSession();
+        // session.setAttribute("username", getRegisterUsername()); //Set session attribute
         //clear the fields
-        registerUsername ="";
-        registerPassword ="";
+        registerUsername = "";
+        registerPassword = "";
         return jsf22Bugfix();
     }
-    
+
     /**
-    * The logoutUser() kills the user session 
-    * 
-    * @return empty string, to handle jsf bug 
-    */
-    public String logoutUser(){
-       startConversation();
+     * The logoutUser() kills the user session
+     *
+     * @return empty string, to handle jsf bug
+     */
+    public String logoutUser() {
+        startConversation();
       // HttpSession session = SessionData.getSession();
-       //session.invalidate();  //Invalidating the session will finally logout the user
-       return jsf22Bugfix();
+        //session.invalidate();  //Invalidating the session will finally logout the user
+        return jsf22Bugfix();
     }
-    
+
     public Exception getException() {
         return loginFailure;
     }
-    
-    
+
     public static String sha256(String base) {
-    try{
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] hash = digest.digest(base.getBytes("UTF-8"));
-        StringBuffer hexString = new StringBuffer();
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(base.getBytes("UTF-8"));
+            StringBuffer hexString = new StringBuffer();
 
-        for (int i = 0; i < hash.length; i++) {
-            String hex = Integer.toHexString(0xff & hash[i]);
-            if(hex.length() == 1) hexString.append('0');
-            hexString.append(hex);
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
         }
-
-        return hexString.toString();
-    } catch(Exception ex){
-       throw new RuntimeException(ex);
     }
-}
-    
-    
-    
+
 }
